8b83d6ea27c031467ae8a7ffe6f056046e411115
Adrian <Adrian.Davis484@outlook.com>
2023-08-17 17:07:43 +0930

    Fully functional coin flip with messages displaying both conditions

--- app.js
+++ app.js
@@ -4,19 +4,27 @@ const coin = document.querySelector('.coin');
 const flipButton = document.getElementById('flip-button');
 
 const flippingMessage = document.getElementById('flipping-message');
-const resultMessage = document.getElementById('result-message'); // Reference the result message element
+const resultMessage = document.getElementById('result-message');
 
 flipButton.addEventListener('click', () => {
+    // Hide the result message and show the flipping message
+    resultMessage.style.display = 'none';
+    flippingMessage.style.display = 'block';
+
     const result = Math.random() < 0.5 ? 'heads' : 'tails';
-    const rotationDuration = 3.0; // Duration of the animation in seconds
+    const rotationDuration = 3.0;
     
     coin.style.animation = `flip-animation-${result} ${rotationDuration}s ease-out forwards`;
     
-    setTimeout(() => {
+    coin.addEventListener('animationend', () => {
+        // Animation ended, update the coin visual and display result message
         coin.style.animation = '';
         coin.setAttribute('data-side', result);
         updateCoinVisual(result);
-    }, rotationDuration * 1000); // Convert seconds to milliseconds
+        flippingMessage.style.display = 'none';
+        resultMessage.style.display = 'block';
+        resultMessage.textContent = result === 'heads' ? 'Heads!' : 'Tails!';
+    }, { once: true }); // Ensure the event listener is called only once
 });
 
 function updateCoinVisual(side) {
